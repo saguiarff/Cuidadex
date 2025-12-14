89ff006764967e56f29bc33111f7e57f0bb80b65
saguiarff <sofiaaguiarff@gmail.com>
2025-12-14 16:34:07 -0400

    Remove pasta backend da branch frontend

--- backend/utils/responses.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-def success(data=None, message="OK", status=200):
-    return jsonify({"success": True, "message": message, "data": data}), status
-
-def error(message="Erro", status=400):
-    return jsonify({"success": False, "error": message}), status
